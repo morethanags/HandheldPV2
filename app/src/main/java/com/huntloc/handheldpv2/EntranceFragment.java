3480935f95d1fd62608a81da45596556530d3436
dmoran <dmoran@huntloc.pe>
2016-09-19 12:25:32 -0500

    Delete unused imports

--- app/src/main/java/com/huntloc/handheldpv2/EntranceFragment.java
+++ app/src/main/java/com/huntloc/handheldpv2/EntranceFragment.java
@@ -15,7 +15,7 @@ public class EntranceFragment extends Fragment implements SwipeRefreshLayout.OnR
     private OnEntranceFragmentInteractionListener mListener;
     private SwipeRefreshLayout swipeRefreshLayout;
     public EntranceFragment() {
-        
+        // Required empty public constructor
     }
 
 
